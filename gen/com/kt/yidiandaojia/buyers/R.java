e22cb74bf3710540b297863a3e4fd374217f08ea
kenzhao <kenzhao@192.168.3.36>
2014-09-11 14:20:01 +0800

    完成tab

--- gen/com/kt/yidiandaojia/buyers/R.java
+++ gen/com/kt/yidiandaojia/buyers/R.java
@@ -15,6 +15,17 @@ public final class R {
         public static final int abc_slide_in_top=0x7f040003;
         public static final int abc_slide_out_bottom=0x7f040004;
         public static final int abc_slide_out_top=0x7f040005;
+        public static final int activityon_down_enter=0x7f040006;
+        public static final int activityon_down_exit=0x7f040007;
+        public static final int activityon_up_enter=0x7f040008;
+        public static final int activityon_up_exit=0x7f040009;
+        public static final int guide_anim=0x7f04000a;
+        public static final int popupwindow_dismiss=0x7f04000b;
+        public static final int popupwindow_show=0x7f04000c;
+        public static final int slide_left_in=0x7f04000d;
+        public static final int slide_left_out=0x7f04000e;
+        public static final int slide_right_in=0x7f04000f;
+        public static final int slide_right_out=0x7f040010;
     }
     public static final class attr {
         /**  Custom divider drawable to use for elements in the action bar. 
@@ -858,10 +869,19 @@ containing a value of this type.
         public static final int abc_split_action_bar_is_narrow=0x7f060002;
     }
     public static final class color {
-        public static final int abc_search_url_text_holo=0x7f070003;
+        public static final int abc_search_url_text_holo=0x7f07000b;
         public static final int abc_search_url_text_normal=0x7f070000;
         public static final int abc_search_url_text_pressed=0x7f070002;
         public static final int abc_search_url_text_selected=0x7f070001;
+        public static final int full_transparent=0x7f070003;
+        public static final int gold=0x7f070006;
+        public static final int popupwindow_bg=0x7f07000a;
+        public static final int progress_background=0x7f070008;
+        public static final int tab_selected_bg=0x7f070009;
+        public static final int tabhost_bg=0x7f070007;
+        public static final int textviewcolor=0x7f07000c;
+        public static final int transparent=0x7f070005;
+        public static final int white=0x7f070004;
     }
     public static final class dimen {
         /**  Default height of an action bar. 
@@ -1078,6 +1098,26 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int ic_launcher=0x7f020057;
+        public static final int icon_139=0x7f020058;
+        public static final int icon_141=0x7f020059;
+        public static final int icon_144=0x7f02005a;
+        public static final int icon_147=0x7f02005b;
+        public static final int icon_154=0x7f02005c;
+        public static final int icon_157=0x7f02005d;
+        public static final int icon_160=0x7f02005e;
+        public static final int icon_162=0x7f02005f;
+        public static final int icon_163=0x7f020060;
+        public static final int icon_165=0x7f020061;
+        public static final int icon_167=0x7f020062;
+        public static final int icon_176=0x7f020063;
+        public static final int selector_tab=0x7f020064;
+        public static final int selector_tab_background=0x7f020065;
+        public static final int tab_home_btn=0x7f020066;
+        public static final int tab_map_btn=0x7f020067;
+        public static final int tab_my_btn=0x7f020068;
+        public static final int tab_shopping_btn=0x7f020069;
+        public static final int tabhost_bg_nol=0x7f02006a;
+        public static final int tabhost_bg_sel=0x7f02006b;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1091,7 +1131,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003c;
+        public static final int action_settings=0x7f050046;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1110,8 +1150,10 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int imageview=0x7f050044;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
+        public static final int ll_main=0x7f05003c;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
@@ -1119,6 +1161,7 @@ containing a value of this type.
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
+        public static final int realtabcontent=0x7f050043;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1136,6 +1179,13 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
+        public static final int tab_content=0x7f05003d;
+        public static final int tab_rb_a=0x7f05003f;
+        public static final int tab_rb_b=0x7f050040;
+        public static final int tab_rb_c=0x7f050041;
+        public static final int tab_rb_d=0x7f050042;
+        public static final int tabs_rg=0x7f05003e;
+        public static final int textview=0x7f050045;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1193,7 +1243,10 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int main=0x7f030019;
+        public static final int main_tab_layout=0x7f03001a;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
+        public static final int tab_item_view=0x7f03001c;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
@@ -2620,7 +2673,7 @@ containing a value of this type.
          */
         public static final int[] MenuView = {
             0x010100ae, 0x0101012c, 0x0101012d, 0x0101012e,
-            0x0101012f, 0x01010130, 0x01010131, 0x01010438
+            0x0101012f, 0x01010130, 0x01010131, 0x01010435
         };
         /**
           <p>
---
+++ src/com/kt/yidiandaojia/buyers/fragment/MapFragment.java
@@ -0,0 +1,7 @@
+package com.kt.yidiandaojia.buyers.fragment;
+
+import android.support.v4.app.Fragment;
+
+public class MapFragment extends Fragment{
+
+}
